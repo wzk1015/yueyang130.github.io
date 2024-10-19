90ca60a7862ab48bc5033acfcffdd23b7aa13975
yueyang130 <yueyang130@gmail.com>
2024-10-19 20:34:19 +0800

    Updates

---
+++ precache-manifest.20227de32a2ea63e5f59e01611effb78.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa629a908493406f3c21f110f2323cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0462923d219bfb8fe8d",
+    "url": "/static/css/main.565a5ff8.chunk.css"
+  },
+  {
+    "revision": "95ea1a714721a3d2d596",
+    "url": "/static/js/2.3895f705.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3895f705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0462923d219bfb8fe8d",
+    "url": "/static/js/main.59a0a5b7.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "b6c4c1b36129b921eb085b42261840db",
+    "url": "/static/media/benchmark.b6c4c1b3.jpg"
+  },
+  {
+    "revision": "1279ea64584aa194964f9be9cc1c89ad",
+    "url": "/static/media/deer.1279ea64.jpg"
+  },
+  {
+    "revision": "2f89a9f056356a1f50b3d416854042cd",
+    "url": "/static/media/odpr.2f89a9f0.jpg"
+  },
+  {
+    "revision": "06871e646decf304a4751dcd650aa7b9",
+    "url": "/static/media/person.06871e64.jpeg"
+  },
+  {
+    "revision": "9c521fd47aa0248aef9a24a18aff1381",
+    "url": "/static/media/person2.9c521fd4.jpg"
+  },
+  {
+    "revision": "c6136b18cc79e1ecccaeda4f893fab5e",
+    "url": "/static/media/physical_law.c6136b18.jpg"
+  },
+  {
+    "revision": "8f9f079d77b7f3fe82012717e9a2f3cc",
+    "url": "/static/media/red.8f9f079d.jpg"
+  },
+  {
+    "revision": "54380c5ae710b8dd78758174cd96a98b",
+    "url": "/static/media/seem.54380c5a.png"
+  },
+  {
+    "revision": "d4a250d1543618b48e7830ac31a24772",
+    "url": "/static/media/soccer.d4a250d1.jpg"
+  },
+  {
+    "revision": "cd2a77b68c4727db9114ad3d65669f9a",
+    "url": "/static/media/surgery.cd2a77b6.jpg"
+  },
+  {
+    "revision": "49ef39b6893bb8dfc0a14a523a90eaf7",
+    "url": "/static/media/vcr.49ef39b6.jpg"
+  }
+]);
